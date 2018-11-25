e9478697b82e5c14dbe012224958f8d67d361f74
Richard Hai <hirichardhai@gmail.com>
2018-11-24 23:04:39 -0800

    edited formcontainer.js

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -17,16 +17,9 @@ class FormContainer extends Component {
   render() {
     const { seo_title } = this.state;
     return (
-      <form id="article-form">
-        <Input
-          text="SEO title"
-          label="seo_title"
-          type="text"
-          id="seo_title"
-          value={seo_title}
-          handleChange={this.handleChange}
-        />
-      </form>
+      <div>
+        formContainer component
+      </div>
     );
   }
 }
